d616d9212b88141ce1de4677a35615c202389369
MrBirender <birenderyadav0918@gmail.com>
2025-02-07 18:04:52 +0530

    add a test.js file

---
+++ test.js
@@ -0,0 +1,6 @@
+/* This branch is create direct from the main branch so it will have all the code which was in the main branch
+
+cmd to create a new branch based on main branch
+and without checking out from the current branch.
+git branch "new branch" 'main branch'
+*/
