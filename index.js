cf65cba014a5cac4c02fc18d6e142ae08def1f54
MrBirender <birenderyadav0918@gmail.com>
2025-02-08 11:42:46 +0530

    error resolved

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log('hai i am from the main branch');
 
 console.log(`This code will break the production code`);
+
+/* bug given by the boss to resolve */
+function sayHello() {
+    console.log('hello');
+}
+
+sayHello();
