41af50530ae94c2989aeaa0d8c750a564283f240
MrBirender <birenderyadav0918@gmail.com>
2025-02-08 12:31:58 +0530

    updated git notes

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ console.log('hai i am from the main branch');
 
 console.log(`This code will break the production code`);
 
+/* working on something important */
+function getUserData(data){
+    console.log(`The user data is here ${data}`);
+}
+
+getUserData('data');
+/* now i have to work on an erro given by the boss
+so i will use git stash */
+
 /* bug given by the boss to resolve */
 function sayHello() {
     console.log('hello');
